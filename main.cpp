93d90e0d7104707bbbb89d16a5d6d3bfd4ea8547
lo4rt <lightroomgs@gmail.com>
2026-05-11 22:15:39 +0500

    Add SDL3 window initialization

--- main.cpp
+++ main.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
 
+#include <SDL3/SDL.h>
 
-int main() {
+int main(int argc, char* argv[]){
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        SDL_Log("Ошибка инициализации SDL: %s", SDL_GetError());
+        return 1;
+    }
 
-    std::cout << "Hello, world!\n";
+    SDL_Window* window = SDL_CreateWindow("SDL3 Test", 640, 480, SDL_WINDOW_VULKAN);
+    if (!window) {
+        SDL_Log("Ошибка создания окна: %s", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Delay(3000);
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 
     return 0;
 }
